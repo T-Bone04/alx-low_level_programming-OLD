3d7476a518eb1f0cf3084b28f71f65ba3cb25761
T-Bone04 <tashen.jason@gmail.com>
2023-04-08 16:54:54 -0700

    Write a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,32 @@
-#include <stdio.h>
-#include "main.h"
-
 /**
- * main - check the code
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a pointer to a string of 0 and 1 characters representing a binary number
  *
- * Return: Always 0.
+ * Return: the converted unsigned int, or 0 if `b` is NULL
+ * or contains non-binary characters
  */
-int main(void)
+unsigned int binary_to_uint(const char *b)
 {
-    unsigned int n;
+	unsigned int result = 0;	/* variable to store the converted unsigned int */
+	int i = 0;	/* counter variable for iterating through the binary string */
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
+	/* check for NULL input */
+	if (!b)
+		return (0);
+
+	/* iterate through the binary string */
+	while (b[i])
+	{
+		/* check for non-binary characters */
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		/* shift result left and add the current binary digit */
+		result <<= 1;
+		result += b[i] - '0';
+
+		i++;	/* increment counter */
+	}
+
+	return (result);	/* return the converted unsigned int */
 }

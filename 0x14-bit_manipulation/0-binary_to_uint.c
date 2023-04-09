e80b7e95117798aac1930c2153c22fd4491de37e
T-Bone04 <tashen.jason@gmail.com>
2023-04-09 13:48:24 -0700

    Betty error checker 5

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,8 @@
  *
  * @b: Pointer to a string containing a binary number
  *
- * Return: The converted unsigned integer, or 0 if b contains invalid characters
+ * Return: The converted unsigned integer,
+ * or 0 if b contains invalid characters
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -15,16 +16,20 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL) /* Check if b is NULL */
 		return (0); /* Return 0 if b is NULL */
 
-	while (b[index] != '\0') /* Loop through each character in b until the null terminator is reached */
+	while (b[index] != '\0') /* Loop through each character in b */
+		/* until the null terminator is reached */
 	{
-		if (b[index] != '0' && b[index] != '1') /* Check if the current character is not a valid binary digit */
-			return (0); /* Return 0 if the current character is not a valid binary digit */
+		if (b[index] != '0' && b[index] != '1')
+		/* Check if the current character is not a valid binary digit */
+			return (0);
+		/* Return 0 if the current character is not a valid binary digit */
 
 		result <<= 1; /* Shift the current value of result one bit to the left */
 		if (b[index] == '1') /* If the current character is a 1 */
 			result += 1; /* Add 1 to the current value of result */
 
-		index++; /* Increment the index variable to move to the next character in b */
+		index++; /* Increment the index variable to */
+		/* move to the next character in b */
 	}
 
 	return (result); /* Return the final converted value of result */

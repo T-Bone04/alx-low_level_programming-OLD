6b4ab496e387cc5d1e145b727ad7a0551ecbdc10
T-Bone04 <tashen.jason@gmail.com>
2023-04-10 13:57:08 -0700

    betty 2-get

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
 
-/* Check if index is greater than the number of bits in unsigned long int */
+	/* Check if index is greater than the number of bits in unsigned long int */
 	if (index >= (sizeof(unsigned long int) * 8))
 	return (-1);
 
-/* Shift 1 by index to the left to get a mask with only the bit at index set */
+	/* Shift 1 by index to left to get a mask with only the bit at index set */
 	mask = 1UL << index;
 	/* Use bitwise AND to check if the bit at index is set */
 	if (n & mask)

c48ffd362ebbdbcb4c427dc475adcea0681f812e
T-Bone04 <tashen.jason@gmail.com>
2023-04-10 13:46:14 -0700

    3-set_bit.c betty error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,17 +7,17 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int mask;
+	unsigned long int mask;
 
-    /* Check if index is greater than the number of bits in unsigned long int */
-    if (index >= (sizeof(unsigned long int) * 8))
-        return (-1);
+	/* Check if index is greater than the number of bits in unsigned long int */
+	if (index >= (sizeof(unsigned long int) * 8))
+	return (-1);
 
-    /* Create a mask with only the bit at index set to 1 */
-    mask = 1UL << index;
+	/* Create a mask with only the bit at index set to 1 */
+	mask = 1UL << index;
 
-    /* Use bitwise OR to set the bit at index to 1 */
-    *n |= mask;
+	/* Use bitwise OR to set the bit at index to 1 */
+	*n |= mask;
 
-    return (1);
+	return (1);
 }
